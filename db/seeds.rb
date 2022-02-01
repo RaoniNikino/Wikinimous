d9e87098489b97d62d50b6b76d889c8916865368
RaoniPirat <raoni.pirat@gmail.com>
2022-02-01 17:09:38 +0100

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Article.create(title: Faker::Book.title.to_s, content: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 10))
+end
